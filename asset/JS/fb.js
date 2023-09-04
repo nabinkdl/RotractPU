0f5781e167f1f5125f173071f70c8dfa09120c36
Nabin Kandel <nabinkandel@student.pu.edu.np>
2023-09-04 09:38:26 +0545

    flex-join us

--- asset/JS/fb.js
+++ asset/JS/fb.js
@@ -6,6 +6,11 @@ function formtwo(){
 
 
 
+  const emailInput = document.getElementById('email');
+  const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+  const testy2 = emailInput.value;
+  const check = emailRegex.test(emailInput.value);
+
 
   form.addEventListener('submit', e => {
     e.preventDefault()
@@ -13,13 +18,26 @@ function formtwo(){
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
 
-      rest();
+      waling();
   })
 
 
   function rest(){ 
-    join.innerHTML = "submited",msg2.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;Thank you for your feedback";
+    join.innerHTML = "Join Us",msg2.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;Being a Rotract means making an impact.";
     }
+
+
+
+
+    function waling(){
+      if(emailRegex.test(emailInput.value) ){join.innerHTML = "Submited",msg2.innerHTML = "<span style='color: green; margin-inline: 16px auto;' >Thank you for your feedback";}
+
+      else {msg2.innerHTML = "<span style='color: red; margin-inline: 20px auto;' >Enter a valid Email !!"}
+      
+      setTimeout(rest, 3000);
+      }
+
+
 
 
 }
